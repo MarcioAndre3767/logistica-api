ec7e361cb6b5f132e1003b3d2179eb6ddcb20b5c
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-08-12 06:02:20 -0300

    implementação das Handler Exception

--- src/main/java/com/devmam/logistica/domain/model/Cliente.java
+++ src/main/java/com/devmam/logistica/domain/model/Cliente.java
@@ -1,6 +1,5 @@
 package com.devmam.logistica.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
